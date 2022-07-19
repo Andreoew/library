d16088201834060623c8ec11fdc23506318be814
André de Souza <andreoew@hotmail.com>
2022-07-18 22:43:54 -0300

    update treatments errors

--- http-validation.js
+++ http-validation.js
@@ -1,21 +1,37 @@
 const fetch = require('node-fetch');
 
+function handleErrors(err) {
+  throw new Error(err.message);
+}
+
 async function checksStates(arraysUrlsLinks) {
-  const stateLinks = await Promise.all(arraysUrlsLinks.map(async url => {
-    const res = await fetch(url)
-    return res.status;
-  }))
-  return stateLinks;
+  try {
+    const stateLinks = await Promise
+      .all(arraysUrlsLinks
+        .map(async url => {
+          const res = await fetch(url)
+          return res.status;
+        }))
+    return stateLinks;
+  } catch (err) {
+    handleErrors(err)
+  }
 }
 
 function createUrls(arrayslinks) {
-  return arrayslinks.map(ObjectLink => Object.values(ObjectLink).join());
+  return arrayslinks
+    .map(ObjectLink => Object
+      .values(ObjectLink).join());
 }
 
 async function handleValidationsUrls(arrayslinks) {
   const links = createUrls(arrayslinks);
   const statesLinks = await checksStates(links);
-  return statesLinks;
+
+  const results = arrayslinks.map((object, index) => ({
+    ...object, states: statesLinks[index]
+  }))
+  return results
 }
 
 module.exports = handleValidationsUrls;
