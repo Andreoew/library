901318cb3a1986e1ef1b642b78cc24c852ff6281
André de Souza <andreoew@hotmail.com>
2022-07-18 22:22:11 -0300

    update created and validated array links

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ const handleValidationsUrls = require('./http-validation');
 async function processContent(pathName) {
   const result = await getFile(pathName[2]);
   if (pathName[3] === 'validation') {
-    console.log(chalk.yellow('links valided'), handleValidationsUrls(result));
+    console.log(chalk.yellow('links valided'), await handleValidationsUrls(result));
   } else {
     console.log(chalk.yellow('Lista de links '), result);
   }
--- http-validation.js
+++ http-validation.js
@@ -1,9 +1,21 @@
-function createUrls(links){
-  return links.map(ObjectLink => Object.values(ObjectLink).join());
+const fetch = require('node-fetch');
+
+async function checksStates(arraysUrlsLinks) {
+  const stateLinks = await Promise.all(arraysUrlsLinks.map(async url => {
+    const res = await fetch(url)
+    return res.status;
+  }))
+  return stateLinks;
 }
 
-function handleValidationsUrls(links){
-  return createUrls(links);
+function createUrls(arrayslinks) {
+  return arrayslinks.map(ObjectLink => Object.values(ObjectLink).join());
+}
+
+async function handleValidationsUrls(arrayslinks) {
+  const links = createUrls(arrayslinks);
+  const statesLinks = await checksStates(links);
+  return statesLinks;
 }
 
 module.exports = handleValidationsUrls;
